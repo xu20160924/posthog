34496e431b300c95a353b95d27085e6f132e8f5d
Ted Kaemming <65315+tkaemming@users.noreply.github.com>
2023-11-14 16:18:09 -0800

    Work in progress on removing the remainder of the mapping tests.

--- posthog/models/test/test_person_override_model.py
+++ posthog/models/test/test_person_override_model.py
@@ -8,7 +8,7 @@ from django.db.utils import DEFAULT_DB_ALIAS, ConnectionHandler, IntegrityError
 
 from posthog.api.test.test_organization import create_organization
 from posthog.api.test.test_team import create_team
-from posthog.models import Person, PersonOverride, PersonOverrideMapping, Team
+from posthog.models import Person, PersonOverride, Team
 
 
 @pytest.fixture
@@ -55,45 +55,28 @@ def oldest_event():
 def test_person_override_disallows_same_old_person_id(team, oldest_event):
     """Test a new old_person_id cannot match an existing old_person_id.
 
-    This is enforced by a UNIQUE constraint on (team_id, old_person_id)
+    This should be enforced by a UNIQUE constraint on (team_id, old_person_id)
     """
     old_person_id = uuid4()
     override_person_id = uuid4()
     new_override_person_id = uuid4()
 
-    old_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=old_person_id,
-    )
-    override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=override_person_id,
-    )
-    person_override = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=team,
-        old_person_id=old_mapping,
-        override_person_id=override_mapping,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
-    )
-    person_override.save()
-
-    assert person_override.old_person_id == old_mapping
-    assert person_override.override_person_id == override_mapping
-
-    new_override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=new_override_person_id,
     )
 
     with pytest.raises(IntegrityError):
         PersonOverride.objects.create(
             team=team,
-            old_person_id=old_mapping,
-            override_person_id=new_override_mapping,
+            old_person_id=old_person_id,
+            override_person_id=new_override_person_id,
             oldest_event=oldest_event,
             version=1,
-        ).save()
+        )
 
 
 @pytest.mark.django_db(transaction=True)
@@ -106,53 +89,25 @@ def test_person_override_same_old_person_id_in_different_teams(organization, tea
         api_token="a different token",
     )
 
-    old_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=old_person_id,
-    )
-    override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=override_person_id,
-    )
-
-    p1 = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=team,
-        old_person_id=old_mapping,
-        override_person_id=override_mapping,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
     )
-    p1.save()
 
-    assert p1.old_person_id == old_mapping
-    assert p1.override_person_id == override_mapping
-
-    new_team_old_mapping = PersonOverrideMapping.objects.create(
-        team_id=new_team.id,
-        uuid=old_person_id,
-    )
-    new_team_override_mapping = PersonOverrideMapping.objects.create(
-        team_id=new_team.id,
-        uuid=override_person_id,
-    )
-
-    p2 = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=new_team,
-        old_person_id=new_team_old_mapping,
-        override_person_id=new_team_override_mapping,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
     )
-    p2.save()
-
-    assert p1.old_person_id.uuid == p2.old_person_id.uuid
-    assert p1.override_person_id.uuid == p2.override_person_id.uuid
-    assert p1.old_person_id.id != p2.old_person_id.id
-    assert p1.override_person_id.id != p2.override_person_id.id
-    assert p1.team != p2.team
 
 
 @pytest.mark.django_db(transaction=True)
+@pytest.mark.xfail()  # TODO: re-evaluate this, as we can no longer enforce this constraint
 def test_person_override_disallows_override_person_id_as_old_person_id(team, oldest_event):
     """Test a new old_person_id cannot match an existing override_person_id.
 
@@ -163,40 +118,22 @@ def test_person_override_disallows_override_person_id_as_old_person_id(team, old
     override_person_id = uuid4()
     new_override_person_id = uuid4()
 
-    old_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=old_person_id,
-    )
-    override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=override_person_id,
-    )
-
-    person_override = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=team,
-        old_person_id=old_mapping,
-        override_person_id=override_mapping,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
-    )
-    person_override.save()
-
-    assert person_override.old_person_id == old_mapping
-    assert person_override.override_person_id == override_mapping
-
-    new_override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=new_override_person_id,
     )
 
     with pytest.raises(IntegrityError):
         PersonOverride.objects.create(
             team=team,
-            old_person_id=override_mapping,
-            override_person_id=new_override_mapping,
+            old_person_id=override_person_id,
+            override_person_id=new_override_person_id,
             oldest_event=oldest_event,
             version=1,
-        ).save()
+        )
 
 
 @pytest.mark.django_db(transaction=True)
@@ -212,50 +149,25 @@ def test_person_override_allows_override_person_id_as_old_person_id_in_different
         api_token="a much different token",
     )
 
-    old_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=old_person_id,
-    )
-    override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=override_person_id,
-    )
-
-    p1 = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=team,
-        old_person_id=old_mapping,
-        override_person_id=override_mapping,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
     )
-    p1.save()
 
-    assert p1.old_person_id == old_mapping
-    assert p1.override_person_id == override_mapping
-
-    new_team_old_mapping = PersonOverrideMapping.objects.create(
-        team_id=new_team.id,
-        uuid=override_person_id,
-    )
-    new_team_override_mapping = PersonOverrideMapping.objects.create(
-        team_id=new_team.id,
-        uuid=new_override_person_id,
-    )
-    p2 = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=new_team,
-        old_person_id=new_team_old_mapping,
-        override_person_id=new_team_override_mapping,
+        old_person_id=override_person_id,
+        override_person_id=new_override_person_id,
         oldest_event=oldest_event,
         version=1,
     )
-    p2.save()
-
-    assert p1.override_person_id.uuid == p2.old_person_id.uuid
-    assert p2.override_person_id == new_team_override_mapping
-    assert p1.team != p2.team
 
 
 @pytest.mark.django_db(transaction=True)
+@pytest.mark.xfail()  # TODO: re-evaluate this, as we can no longer enforce this constraint
 def test_person_override_disallows_old_person_id_as_override_person_id(team, oldest_event):
     """Test a new override_person_id cannot match an existing old_person_id.
 
@@ -266,45 +178,22 @@ def test_person_override_disallows_old_person_id_as_override_person_id(team, old
     override_person_id = uuid4()
     new_old_person_id = uuid4()
 
-    old_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=old_person_id,
-    )
-    old_mapping.save()
-
-    override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=override_person_id,
-    )
-    override_mapping.save()
-
-    person_override = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=team,
-        old_person_id=old_mapping,
-        override_person_id=override_mapping,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
     )
-    person_override.save()
-
-    assert person_override.old_person_id == old_mapping
-    assert person_override.override_person_id == override_mapping
-
-    new_old_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=new_old_person_id,
-    )
-    new_old_mapping.save()
 
     with pytest.raises(IntegrityError):
-        p = PersonOverride.objects.create(
+        PersonOverride.objects.create(
             team=team,
-            old_person_id=new_old_mapping,
-            override_person_id=old_mapping,
+            old_person_id=new_old_person_id,
+            override_person_id=old_person_id,
             oldest_event=oldest_event,
             version=1,
         )
-        p.save()
 
 
 @pytest.mark.django_db(transaction=True)
@@ -318,49 +207,21 @@ def test_person_override_old_person_id_as_override_person_id_in_different_teams(
         api_token="a significantly different token",
     )
 
-    old_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=old_person_id,
-    )
-    override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=override_person_id,
-    )
-
-    p1 = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=team,
-        old_person_id=old_mapping,
-        override_person_id=override_mapping,
+        old_person_id=old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
     )
-    p1.save()
 
-    assert p1.old_person_id == old_mapping
-    assert p1.override_person_id == override_mapping
-
-    new_old_mapping = PersonOverrideMapping.objects.create(
-        team_id=new_team.id,
-        uuid=new_old_person_id,
-    )
-    new_override_mapping = PersonOverrideMapping.objects.create(
-        team_id=new_team.id,
-        uuid=old_mapping.uuid,
-    )
-
-    p2 = PersonOverride.objects.create(
+    PersonOverride.objects.create(
         team=new_team,
-        old_person_id=new_old_mapping,
-        override_person_id=new_override_mapping,
+        old_person_id=new_old_person_id,
+        override_person_id=override_person_id,
         oldest_event=oldest_event,
         version=1,
     )
-    p2.save()
-
-    assert p1.old_person_id.uuid == p2.override_person_id.uuid
-    assert p1.old_person_id.team_id == p1.override_person_id.team_id
-    assert p2.old_person_id == new_old_mapping
-    assert p1.team != p2.team
 
 
 @pytest.mark.django_db(transaction=True)
@@ -370,31 +231,19 @@ def test_person_override_allows_duplicate_override_person_id(team, oldest_event)
     n_person_overrides = 2
     created = []
 
-    override_mapping = PersonOverrideMapping.objects.create(
-        team_id=team.id,
-        uuid=override_person_id,
-    )
-
     for _ in range(n_person_overrides):
         old_person_id = uuid4()
-        old_mapping = PersonOverrideMapping.objects.create(
-            team_id=team.id,
-            uuid=old_person_id,
-        )
-
         person_override = PersonOverride.objects.create(
             team=team,
-            old_person_id=old_mapping,
-            override_person_id=override_mapping,
+            old_person_id=old_person_id,
+            override_person_id=override_person_id,
             oldest_event=oldest_event,
             version=1,
         )
-        person_override.save()
 
         created.append(person_override)
 
-    assert all(p.override_person_id == override_mapping for p in created)
-    assert len(set(p.old_person_id.uuid for p in created)) == n_person_overrides
+    assert len(set(p.old_person_id for p in created)) == n_person_overrides
 
 
 @contextlib.contextmanager
